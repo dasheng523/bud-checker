aa76fdd046b00c6a0df5ba9436b99c6f65f0d279
yesheng <dasheng523@163.com>
2023-12-03 20:22:34 +0800

    增加一些内置方法

--- src/main/java/com/mengxinya/ys/funcgetter/FunctionGetterBaseImpl.java
+++ src/main/java/com/mengxinya/ys/funcgetter/FunctionGetterBaseImpl.java
@@ -1,5 +1,6 @@
 package com.mengxinya.ys.funcgetter;
 
+import com.alibaba.fastjson.JSON;
 import com.mengxinya.ys.common.Evaluator;
 
 import java.util.List;
@@ -11,7 +12,10 @@ public class FunctionGetterBaseImpl implements FunctionGetter {
             case "abs" -> input -> Math.abs(Double.parseDouble(input.get(0).toString()));
             case "isString" -> input -> input.get(0) instanceof String;
             case "len" -> input -> input.get(0).toString().length();
-            case "notnull" -> input -> input.get(0) != null;
+            case "notnull", "notNull" -> input -> input.get(0) != null;
+            case "isNull" -> input -> input.get(0) == null;
+            case "regex" -> input -> input.get(0).toString().matches(input.get(1).toString());
+            case "inArray" -> input -> input.subList(1, input.size()).contains(input.get(0));
             default -> null;
         };
     }
